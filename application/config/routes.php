134c7d3e1d5b35eacd63009fd17e3fadc86fa000
NB-HP\anon de <anon_de@saijo-denki.co.th>
2019-06-27 16:35:47 +0700

    Update Province list

--- application/config/routes.php
+++ application/config/routes.php
@@ -100,6 +100,8 @@ $route['api/v1/orz-group'] = "organized/getOrzGroupList";
 $route['api/v1/orz-register'] = "organized/organization_register";
 $route['api/v1/orz-update'] = "organized/organization_update";
 $route['api/v1/orz-reg-last'] = "organized/getOrganizationLast";
+$route['api/v1/province-list'] = "api/provinceList";
+
 
 
 
